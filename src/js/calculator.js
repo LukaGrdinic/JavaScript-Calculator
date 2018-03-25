3ee4c22b77bcbb2b0d007e26c378decdaa51d37d
LukaGrdinic <1lukagrdinic@gmail.com>
2018-03-25 14:37:51 +0200

    keybord listeners added
    Added keybord listeners to calculator.js - Now user can type keys and do calculations.
    Cleaned up some html,css and js.

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,109 +1,163 @@
-/* THIS IS THE MAIN OBJECT - CALCULATOR */
+var resultElement = document.getElementById("result"); // HTML element where result is displayed
+var currentOperatorElement = document.getElementById('currentOperator'); // HTML element where currentOperator is displayed
+
+/* THE CALCULATOR OBJECT */
 /* ==================================== */
 
 let calculator = {
 
-    stringifiedNumber: "", // IT ALLOWS THE CIPHERS TO CONCATINATE INTO NUMBERS OF MORE DIGITS (25, 155, 300, 999, 125000)
+    stringifiedNumber: '', // It allows the ciphers to concatenate into numbers of more digits ('25', '155', '300', '999', '125000')
     assignToString: function (x) {
         this.stringifiedNumber += x;
     },
     stringParser: function () {
-        let parsedNumber = parseFloat(this.stringifiedNumber); // ODJE CE MOZDA TREBATI parseFloat
+        if (this.stringifiedNumber === '' || this.stringifiedNumber === '.') {
+            return 0; // returns a 0 instead of returning NaN when attempting to parse an empty string ('') or a dot ('.')
+        }
+        let parsedNumber = parseFloat(this.stringifiedNumber);
         return parsedNumber;
     },
     clearStringifiedNumber: function () {
-        this.stringifiedNumber = "";
+        if (this.showFinalResultStatus) {
+            this.clearAll();
+        } else {
+            this.stringifiedNumber = '';
+        }
+        resultElement.innerText = this.calculationArray[0];
     },
     clearAll: function () {
-        // MISLIM DA TREBA ODJE DA SE PROMIJENI I currentOperator u 'add'
-        this.currentOperator = 'add';
-        this.stringifiedNumber = "";
-        this.myArray[0] = 0; // CLEARS UP THE ACCUMULATOR
-        this.myArray[1] = 0; // CLEARS UP THE CURRENT NUMBER
+        this.currentOperator = '+';
+        this.stringifiedNumber = '';
+        this.calculationArray[0] = 0; // Clears up the accumulator
+        this.calculationArray[1] = 0; // Clears up the current number
+        resultElement.innerText = this.calculationArray[0];
+        currentOperatorElement.innerText = '';
     },
-    myArray: [0, 0], // MYARRAY[0] (ACCUMULATOR) IS THE RESULT & MYARRAY[1] IS THE CURRENT NUMBER
-    currentOperator: 'add',
+    // Maybe this array should have been split up into accumulator and currentNumber variables
+    calculationArray: [0, 0], // calculationArray[0] (accumulator) is the result & calculationArray[1] is the current number
+    currentOperator: '+',
     calculateFinalResult: function () {
-        let finalResult = calculator.myArray.reduce(function (a, b) {
+        let finalResult = calculator.calculationArray.reduce(function (a, b) {
             switch (calculator.currentOperator) {
-                case 'add': return a + b; break;
-                case 'subtract': return a - b; break;
-                case 'multiply': return a * b; break;
-                case 'divide': return a / b; break;
-                case 'findPercent': return a / 100 * b; break;
+                case '+': return a + b; break; // DA LI JE OVO BREAK POTREBNO ODJE ?
+                case '-': return a - b; break;
+                case 'x': return a * b; break;
+                case '÷': return a / b; break;
+                case '%': return a / 100 * b; break;
             }
         });
-
-        calculator.myArray[0] = finalResult;
-        console.log(finalResult);
+        calculator.calculationArray[0] = finalResult;
+        resultElement.innerText = calculator.calculationArray[0];
     },
     showFinalResult: function () {
-        calculator.myArray[1] = calculator.stringParser(this.stringifiedNumber);
+        if (this.showFinalResultStatus) { return; }
+        calculator.calculationArray[1] = calculator.stringParser(this.stringifiedNumber);
         calculator.calculateFinalResult();
-        // ODJE STAVLJAMO stringifiedNumber = "", DA BI GA OPERATORI PREPOZNALI NAKON STO KLIKNEMO ZNAK =
-        calculator.stringifiedNumber = "";
-    }
+        calculator.stringifiedNumber = ''; // maybe this expression could be implemented in calculateFinalResult() function
+        this.showFinalResultStatus = true;
+        currentOperatorElement.innerText = '';
+    },
+    showFinalResultStatus: false
 };
 
-/* THIS IS THE HANDLERS OBJECT */
+/* THE HANDLERS OBJECT */
 /* ==================================== */
 
 let handlers = {
-    setUpEventListeners: function () {
+    setUpButtonListeners: function () {
         let calculatorBody = document.querySelector('.calculator-body');
-        calculatorBody.addEventListener('click', function (e) {            // MOZDA OVO TREBA DA ZAMIJENIM TAKO STO CU SAMO U HTML DA STAVIM ONCLICK NA SVAKO DUGME
+        calculatorBody.addEventListener('click', function (e) {
             if (e.target.classList.length === 0) {
                 console.log('Ain\'t nottin\' gonna happen');
             } else {
                 e.target.classList.forEach(function (a) {
-                    if (a === 'valid') {
+                    if (a === 'validNumber') {
                         handlers.buttonNumber(e.target.textContent.trim());
                     } else if (a === 'operator') {
                         handlers.buttonOperator(e.target.textContent.trim());
                     }
+                    e.target.blur();    /* It removes the focus from the last clicked button,
+                     so that when user types ENTER key on keybord it does not click the button again*/
                 });
             }
         });
     },
-    buttonNumber: function (number) {         // AKO JE KLIKNUT BROJ
-        calculator.assignToString(number);
-        console.log(calculator.stringifiedNumber);
-    },
-    setCurrentOperator: function (operator) {
-        switch (operator) {
-            case '+': calculator.currentOperator = 'add'; break;
-            case '-': calculator.currentOperator = 'subtract'; break;
-            case '*': calculator.currentOperator = 'multiply'; break;
-            case '/': calculator.currentOperator = 'divide'; break;
-            case '%': calculator.currentOperator = 'findPercent'; break;
+    buttonNumber: function (number) { // If number button is clicked
+        if (calculator.showFinalResultStatus) {
+            calculator.clearAll();
         }
+        calculator.showFinalResultStatus = false;
+        calculator.assignToString(number);
+        resultElement.innerText = calculator.stringifiedNumber;
     },
-    buttonOperator: function (operator) {        // AKO JE KLIKNUT OPERATOR
-        // AKO JE stringifiedNumber = "", ONDA SE IZVRSAVA SAMO PROMJENA OPERATORA A NE VRSI SE KALKULISANJE REZULTATA...
-        if (calculator.stringifiedNumber === "") {
+    buttonOperator: function (operator) { // If operator button is clicked
+        calculator.showFinalResultStatus = false;
+        if (calculator.stringifiedNumber === '') {
             handlers.setCurrentOperator(operator);
         }
-        // AKO JE stringifiedNumber != "", ONDA SE IZVRSAVA I PROMJENA OPERATORA A I KALKULISANJE REZULTATA...
         else {
-            calculator.myArray[1] = calculator.stringParser(this.stringifiedNumber);
-            calculator.stringifiedNumber = "";
+            calculator.calculationArray[1] = calculator.stringParser(this.stringifiedNumber);
+            calculator.stringifiedNumber = '';
             calculator.calculateFinalResult();
             handlers.setCurrentOperator(operator);
         }
+    },
+    setCurrentOperator: function (operator) {
+        switch (operator) {
+            case '+': calculator.currentOperator = '+'; break;
+            case '-': calculator.currentOperator = '-'; break;
+            case 'x': calculator.currentOperator = 'x'; break;
+            case '÷': calculator.currentOperator = '÷'; break;
+            case '%': calculator.currentOperator = '%'; break;
+        }
+        currentOperatorElement.innerText = calculator.currentOperator; // This displays the current (active) operator
     }
 }
 
-// SETTING UP THE EVENT LISTENERS
-handlers.setUpEventListeners();
+// Setting up button listeners
+handlers.setUpButtonListeners();
+
+// Setting up keybord listeners
+document.addEventListener('keyup', function (e) {
+    switch (e.keyCode) {
+
+        /* THE ENTER KEY, THE ESCAPE KEY AND BACKSPACE */
+        //===============================================
+        case 13: calculator.showFinalResult(); break;
+        case 27: calculator.clearAll(); break;
+        case 8: calculator.clearStringifiedNumber(); break;
+
+        /* NUMBERS ON MAIN KEYPAD */                           /* NUMBERS ON NUMERIC KEYPAD */
+        //======================================               //========================================
+        case 48: handlers.buttonNumber(0); break;     /* || */   case 96: handlers.buttonNumber(0); break;
+        case 49: handlers.buttonNumber(1); break;     /* || */   case 97: handlers.buttonNumber(1); break;
+        case 50: handlers.buttonNumber(2); break;     /* || */   case 98: handlers.buttonNumber(2); break;
+        case 51: handlers.buttonNumber(3); break;     /* || */   case 99: handlers.buttonNumber(3); break;
+        case 52: handlers.buttonNumber(4); break;     /* || */  case 100: handlers.buttonNumber(4); break;
+        case 53: handlers.buttonNumber(5); break;     /* || */  case 101: handlers.buttonNumber(5); break;
+        case 54: handlers.buttonNumber(6); break;     /* || */  case 102: handlers.buttonNumber(6); break;
+        case 55: handlers.buttonNumber(7); break;     /* || */  case 103: handlers.buttonNumber(7); break;
+        case 56: handlers.buttonNumber(8); break;     /* || */  case 104: handlers.buttonNumber(8); break;
+        case 57: handlers.buttonNumber(9); break;     /* || */  case 105: handlers.buttonNumber(9); break;
+        case 190: handlers.buttonNumber('.'); break;  /* || */  case 110: handlers.buttonNumber('.'); break;
+
+        // OPERATORS
+        //===========================================
+        case 106: handlers.buttonOperator('x'); break;
+        case 107: handlers.buttonOperator('+'); break;
+        case 109: handlers.buttonOperator('-'); break;
+        case 111: handlers.buttonOperator('÷'); break;
+        default: return;
+    }
+});
+
+function showScientific() {
+    window.alert('Work in progress!');
+}
 
 /* DALJI PLAN ZA DIGITRON */
 
-/* TREBA DA KAD MYARRAY VEC IMA VRIJEDNOSTI, (I MYARRAY[0] I MYARRAY[1])
-A ONDA SAMO UKUCAM BROJ NOVI, NE UKUCAVAJUCI OPERATOR PRIJE BROJA,
-TREBA DA MYOPERATOR === 'add', MYARRAY[0] = 0, A DA NOVI BROJ BUDE MYARRAY[1] */
-// TREBA DA MNOZENJE, DIJELJENJE I NALAZENJE PROCENTA IMA PREDNOST U ODNOSU NA SABIRANJE I ODUZIMANJE
-// MOZDA BI TREBALO PONEKAD DA BACI GRESKU (DA OBAVIJESTI ILI U KOZNOLI ILI NA EKRANU)
 // TREBA SE POBRINUT ZA DECIMALNE BROJEVE, DA FUNKCIONISU KAKO TREBA, JER ZA SAD RADE SAMO PO DEFAULTU
-
-
-// NAPRAVITI INTERFEJS,PRIKAZ BROJEVA DOK SE KUCAJU
+/* ??? OVO CE SE MOZDA BOLJE VIDJETI U TESTOVIMA */
+// MOZDA BI TREBALO PONEKAD DA BACI GRESKU (DA OBAVIJESTI ILI U KONZOLI ILI NA EKRANU)
+// TREBA DA MNOZENJE, DIJELJENJE I NALAZENJE PROCENTA IMA PREDNOST U ODNOSU NA SABIRANJE I ODUZIMANJE
